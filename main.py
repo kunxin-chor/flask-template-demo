634b2ed3280631802cb59f5646d92e70d93bf8b6
Ubuntu <ubuntu@ip-172-31-24-21.ap-southeast-1.compute.internal>
2019-07-10 03:17:27 +0000

    Added layout and about-us page

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/about-us')
+def about_us():
+    return render_template("about-us.html")
+
 # "magic code" -- boilerplate
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
